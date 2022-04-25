d4e7358ae41d06ff1f7b190722c3a731a8fc9de5
Glebson Santos <glebsonsantos@outlook.pt>
2022-04-25 14:40:33 -0300

    Todas questões feitas e funcionando

---
+++ Questão2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Questão2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double acertadas = 0, erradas = 0, perto = 0;
+
+            Console.WriteLine("Bem vindo(a) ao jogo de adinvinhação !!");
+            Console.Write("Digite a quantidade de vezes que deseja jogar: ");
+            int quantidadeJogar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+           
+            for (int i = 0; i < quantidadeJogar; i++)
+            {
+                int numberRandom = new Random().Next(1, 10);
+                Console.Write("Digite um numero entre 1 e 10: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                if(numberRandom == numero)
+                {
+                    Console.WriteLine("Parabens, você acertou. O numero erra: " + numberRandom);
+                    acertadas++;
+                }
+                else if (Math.Abs(numberRandom - numero) == 1)
+                {
+                    Console.WriteLine("Essa passou perto em...,o numero Foi: " + numberRandom);
+                    perto++;
+                }
+                else
+                {
+                    Console.WriteLine("Errou Feio, o numero Foi: " + numberRandom);
+                    erradas++;
+                }
+                Console.WriteLine("-------------------------------------------");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("O numero de Tentativas Foram: " + quantidadeJogar);
+            Console.WriteLine("O numero de vezes Acertadas Foram: " + acertadas);
+            Console.WriteLine("O numero de vezes Erradas Foram: " + erradas);
+            Console.WriteLine("A numero de vezes que Passou Perto Foram: " + perto);
+        }
+    }
+}
---
+++ Questão3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Questão3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            string[] remedio = new string[7]{ "Amoxicilina", "Rivotril", "Xarelto", "Torsilax", "Ozempic", "Dorflex", "Ivermectina" };
+
+            Console.Write("Digite o nome de um remedio: ");
+            string nomeRemedio = Console.ReadLine();
+
+            for (int i = 0; i < remedio.Length; i++)
+            {
+                if (remedio[i] == nomeRemedio)
+                {
+                    Console.WriteLine($"O Remedio {nomeRemedio} estava na possição {i+1} de {nomeRemedio.Length} do array");
+                }
+                else
+                {
+                    // a cada vez que n tem o remedio é adicionado +1 a variavel quando ele percorer todo o array ela
+                    //vai ficar com o msm tamanho do array dai a condição pra so ai ser falado que não existe o remedio
+                    n++;
+                    if(n == remedio.Length)
+                        Console.WriteLine("O remedio informado não se encontra no array !!");
+                }
+            }
+
+            Array.Sort(remedio); // ordenando por ordem alfabetica
+
+            for (int i = 0; i < remedio.Length; i++) //exibindo a posição do remedio onde o array é ordenado
+            {
+                if (remedio[i] == nomeRemedio)
+                {
+                    Console.WriteLine($"O Remedio {nomeRemedio} estava na possição {i + 1} de {nomeRemedio.Length} do array");
+                }
+            }
+        }
+    }
+}
